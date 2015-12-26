6c4cab6bddb0c7afba275f6e1d086702bcfab884
tmtwd <t.williams.im@gmail.com>
2015-12-26 00:52:20 -0500

    completed the component

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,21 @@
-// Create a react component
+// import some code
+var React = require('react-native');
+var AppRegistry = React.AppRegistry;
+var Text = React.Text;
+var View = React.View;
 
+// Create a react component
 var Weekdays = React.createClass({
   render: function(){
-    
+    return <View>
+      <Text>
+        Days Of The Week
+      </Text>
+    </View>
   }
-})
+});
 
 // Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function(){
+  return Weekdays
+});
